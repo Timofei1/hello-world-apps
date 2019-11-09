d5a9e54a72920d783773df9dc88c0e0bb95dfbb4
Timofei1 <57501337+Timofei1@users.noreply.github.com>
2019-11-09 20:36:26 +0400

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -18,7 +18,7 @@ const Home = ({ id, go, fetchedUser }) => (
 		<Group title="Navigation Example">
 			<Div>
 				<Button size="xl" level="2" onClick={go} data-to="persik">
-					Show me the Persik, please
+					Там паук валера
 				</Button>
 			</Div>
 		</Group>
